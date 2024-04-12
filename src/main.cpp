022c87218681e88049225032c3d8a03569b03234
Aldrick T <taldrick@gmail.com>
2024-04-11 23:25:56 -0600

    Updated op. Menu. Added setDist.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 // 3pi+ Auto Roaming
 // All in one functionality test platform.
 // By: aldrick-t
-// Version: 1.0.5 (April 2024)
+// Version: 1.0.6 (April 2024)
 //
 //===============================
 
@@ -10,7 +10,8 @@
 #include <Arduino.h>
 #include <Pololu3piPlus32U4.h>
 #include <string.h>
-//#include <Pololu3piPlus32U4IMU.h>
+#include <Pololu3piPlus32U4IMU.h>
+#include <Wire.h>
  
 using namespace Pololu3piPlus32U4;
  
@@ -26,11 +27,12 @@ BumpSensors bumpSensors;
 Motors motors;
 Encoders encoders;
 
-int motorSpeed = 80;
+//Global Variables
+int motorSpeed = 90;
 int motorSpeedRev = motorSpeed/1.25;
 int motorSpeedTurn = motorSpeed/2;
-signed long int encCountsL = encoders.getCountsAndResetLeft();
-signed long int encCountsR = encoders.getCountsAndResetRight();
+signed long encCountsL = encoders.getCountsAndResetLeft();
+signed long encCountsR = encoders.getCountsAndResetRight();
 bool bumpLeft = false;
 bool bumpRight = false;
 uint16_t lineSensVals[5];
@@ -47,6 +49,7 @@ const char forwardArrows[] PROGMEM = {
   0b00000,
 };
 
+//Fully lit up character matrix.
 const char fullBlock[] PROGMEM = {
   0b11111,
   0b11111,
@@ -82,18 +85,13 @@ const char reverseArrows[] PROGMEM = {
   0b00000,
 };
 
-void displayBackArrow()
-{
-  display.gotoXY(5,3);
-  display.print(F("B\7"));
-  display.gotoXY(0,0);
-}
-
+//Menu display declarations
 char mainMenu(char);
 char opMenu(char);
 int settings(int vel);
 void about();
 
+//Settings function declarations
 int speed(int);
 void lineSensorsSet(int);
 void bumpSensorsSet(int);
@@ -102,8 +100,10 @@ void motorsSet(int);
 void inertialSet(int);
 void feedbackSet(int);
 
+//Operation modes declarations
 void turtleAuto();
 void doubtEvents();
+void setDist();
 
 void setup() {
   //loads custom characters to memory
@@ -152,6 +152,11 @@ void loop() {
     case 12:
       //Doubt events
       doubtEvents();
+      mode = 1;
+      break;
+    case 13:
+      //Set Distance
+      setDist();
       mode = 1;
       break;
     case 21:
@@ -233,31 +238,35 @@ char opMenu(char mode) {
   display.clear();
   display.noInvert();
   display.setLayout21x8();
-
   display.gotoXY(0,0);
   display.print("Operation Modes:     ");
   display.gotoXY(0,2);
-  display.print("Turt. Full Auto w/ :A");
-  display.gotoXY(0,3);
-  display.print(" Obs. Avoid & Line   ");
-  display.gotoXY(0,4);
-  display.print("Doubt Events       :B");
+  display.print("                   >>");
   display.gotoXY(0,5);
-  display.print(" Asks user for input ");
+  display.print("Next               :A");
+  display.gotoXY(0,6);
+  display.print("Select             :B");
   display.gotoXY(0,7);
-  display.print("Main Menu\7         :C");
+  display.print("Back\7              :C");
   display.display();
 
+  int setting = 0;
+  String settings[] = {"Turtle Full Auto  ", "Doubt Events      ", "Set Distance      "};
   while(true) {
-    if(buttonA.getSingleDebouncedPress()) {
-      mode = 11;
+    display.gotoXY(0,2);
+    //display.print("                   ");
+    display.gotoXY(0,2);
+    display.print(settings[setting]);
+    //display.displayPartial(2, 0, 23);
+    if (buttonA.getSingleDebouncedPress()){
+      setting++;
+      if (setting == 3) setting = 0;
+    }
+    else if (buttonB.getSingleDebouncedPress()){
+      mode = setting + 11;
       break;
     }
-    else if(buttonB.getSingleDebouncedPress()) {
-      mode = 12;
-      break;
-    }
-    else if(buttonC.getSingleDebouncedPress()) {
+    if(buttonC.getSingleDebouncedPress()) {
       mode = 0;
       break;
     }
@@ -557,6 +566,13 @@ void motorsSet(int sens) {
 }
 
 void inertialSet(int sens) {
+  display.clear();
+  display.setLayout21x8();
+  display.gotoXY(0,0);
+  display.print("Inertial Sensors:    ");
+  display.gotoXY(0,2);
+  display.print("Accelerometer        ");
+  display.gotoXY(0,3);
 
 }
 
@@ -702,71 +718,72 @@ void turtleAuto() {
       avoidCount = 0;
     }
 
-    for(int i=0; i < 5; i++) {
-      lineSensors.readCalibrated(lineSensVals);
-      if(lineSensVals[i] > 650) {
-        motors.setSpeeds(0, 0);
-        encLocL = encoders.getCountsAndResetLeft();
-        encLocR = encoders.getCountsAndResetRight();
-        while (encLocL > -600 && encLocR > -600) {
-          motors.setSpeeds(-motorSpeedRev, -motorSpeedRev);
-          encLocL = encoders.getCountsLeft();
-          encLocR = encoders.getCountsRight();
+    //Edge Detection (Rev + 90Turn Right)
+    if(lineSensVals[0] > 650 && lineSensVals[1] > 650 && lineSensVals[2] > 650 && lineSensVals[3] > 650 && lineSensVals[4] > 650) {
+      
+      for(int i=0; i < 5; i++) {
+        lineSensors.readCalibrated(lineSensVals);
+        if(lineSensVals[i] > 650) {
+          motors.setSpeeds(0, 0);
+          encLocL = encoders.getCountsAndResetLeft();
+          encLocR = encoders.getCountsAndResetRight();
+          while (encLocL > -600 && encLocR > -600) {
+            motors.setSpeeds(-motorSpeedRev, -motorSpeedRev);
+            encLocL = encoders.getCountsLeft();
+            encLocR = encoders.getCountsRight();
+          }
+          encLocL = encoders.getCountsAndResetLeft();
+          encLocR = encoders.getCountsAndResetRight();
+          motors.setSpeeds(0, 0);
+          switch (i) {
+          case 0:
+            encLocL = encoders.getCountsAndResetLeft();
+            encLocR = encoders.getCountsAndResetRight();
+            while (encLocL < 120) {
+              motors.setSpeeds(motorSpeedTurn, -motorSpeedTurn);
+              encLocL = encoders.getCountsLeft();
+              encLocR = encoders.getCountsRight();
+            }
+            motors.setSpeeds(0, 0);
+            break;
+          case 1 || 3:
+            encLocL = encoders.getCountsAndResetLeft();
+            encLocR = encoders.getCountsAndResetRight();
+            while (encLocL < 320) {
+              motors.setSpeeds(motorSpeedTurn, -motorSpeedTurn);
+              encLocL = encoders.getCountsLeft();
+              encLocR = encoders.getCountsRight();
+            }
+            motors.setSpeeds(0, 0);
+            break;
+          case 2:
+            encLocL = encoders.getCountsAndResetLeft();
+            encLocR = encoders.getCountsAndResetRight();
+            while (encLocL < 260) {
+              motors.setSpeeds(motorSpeedTurn, -motorSpeedTurn);
+              encLocL = encoders.getCountsLeft();
+              encLocR = encoders.getCountsRight();
+            }
+            motors.setSpeeds(0, 0);
+            break;
+          case 4:
+            encLocL = encoders.getCountsAndResetLeft();
+            encLocR = encoders.getCountsAndResetRight();
+            while (encLocR < 120) {
+              motors.setSpeeds(-motorSpeedTurn, motorSpeedTurn);
+              encLocL = encoders.getCountsLeft();
+              encLocR = encoders.getCountsRight();
+            }
+            break;
+          default:
+            break;
+          }
         }
-        encLocL = encoders.getCountsAndResetLeft();
-        encLocR = encoders.getCountsAndResetRight();
         motors.setSpeeds(0, 0);
-        switch (i) {
-        case 0:
-          encLocL = encoders.getCountsAndResetLeft();
-          encLocR = encoders.getCountsAndResetRight();
-          while (encLocL < 120) {
-            motors.setSpeeds(motorSpeedTurn, -motorSpeedTurn);
-            encLocL = encoders.getCountsLeft();
-            encLocR = encoders.getCountsRight();
-          }
-          motors.setSpeeds(0, 0);
-          break;
-        case 1 || 3:
-          encLocL = encoders.getCountsAndResetLeft();
-          encLocR = encoders.getCountsAndResetRight();
-          while (encLocL < 320) {
-            motors.setSpeeds(motorSpeedTurn, -motorSpeedTurn);
-            encLocL = encoders.getCountsLeft();
-            encLocR = encoders.getCountsRight();
-          }
-          motors.setSpeeds(0, 0);
-          break;
-        case 2:
-          encLocL = encoders.getCountsAndResetLeft();
-          encLocR = encoders.getCountsAndResetRight();
-          while (encLocL < 260) {
-            motors.setSpeeds(motorSpeedTurn, -motorSpeedTurn);
-            encLocL = encoders.getCountsLeft();
-            encLocR = encoders.getCountsRight();
-          }
-          motors.setSpeeds(0, 0);
-          break;
-        case 4:
-          encLocL = encoders.getCountsAndResetLeft();
-          encLocR = encoders.getCountsAndResetRight();
-          while (encLocR < 120) {
-            motors.setSpeeds(-motorSpeedTurn, motorSpeedTurn);
-            encLocL = encoders.getCountsLeft();
-            encLocR = encoders.getCountsRight();
-          }
-          break;
-        default:
-          break;
-        }
+        //avoidCount++;
       }
-      motors.setSpeeds(0, 0);
-      //avoidCount++;
-    }
-    
+    }  
 
-
-    
     //No Anomalies (Forward)
     if (!bumpSensors.leftIsPressed() && !bumpSensors.rightIsPressed()) {
       ledRed(0);
@@ -790,3 +807,95 @@ void doubtEvents() {
 
 }
 
+void setDist() {
+  display.clear();
+  display.noInvert();
+  display.setLayout21x8();
+  display.gotoXY(0,0);
+  display.print("Set Distance:        ");
+  display.gotoXY(0,1);
+  display.print("Speed:               ");
+  display.gotoXY(0,2);
+  display.print("Distance:            ");
+  display.gotoXY(0,3);
+  display.print("Direction:           ");
+  display.gotoXY(0,5);
+  display.print(" A        B        C ");
+  display.gotoXY(0,6);
+  display.print(" +       SET       - ");
+  display.gotoXY(0,7);
+  //display.print("               Hold B\7");
+
+  int dist = 0;
+  bool dir = true;
+  int speed = 0;
+  int modeLoc = 0;
+  unsigned long prevTime = millis();
+  unsigned long pressTime = 0; 
+  
+  while(true) {
+    // if (buttonB.isPressed()) {
+    //   pressTime = millis();
+    //   if (pressTime - prevTime > 5000) {
+    //     prevTime = 0;
+    //     pressTime = 0;
+    //     break;
+    //   }
+    //   else {
+    //     prevTime = 0;
+    //     pressTime = 0;
+    //   }
+    // }
+    switch (modeLoc) {
+    case 0:
+      while(true) {
+        display.gotoXY(12,1);
+        display.print("->");
+        display.gotoXY(15,1);
+        display.print(speed);
+        display.print(" ");
+        if(buttonA.getSingleDebouncedPress() && speed < 300) {
+          speed = speed + 20;
+        }
+        else if(buttonC.getSingleDebouncedPress() && speed > 0) {
+          speed = speed - 20;
+        }
+        else if(buttonB.getSingleDebouncedPress()) {
+          display.gotoXY(0,1);
+          display.print("Speed:         ");
+          modeLoc++;
+          break;
+        }
+      }
+      break;
+    case 1:
+      while(true) {
+        display.gotoXY(12,2);
+        display.print("->");
+        display.gotoXY(15,2);
+        display.print(dist);
+        display.print("   ");
+        display.gotoXY(20,2);
+        display.print("cm");
+        if(buttonA.getSingleDebouncedPress() && dist < 9999) {
+          dist = dist + 20;
+        }
+        else if(buttonC.getSingleDebouncedPress() && dist > 0) {
+          dist = dist - 20;
+        }
+        else if(buttonB.getSingleDebouncedPress()) {
+          display.gotoXY(0,2);
+          display.print("Distance:      ");
+          modeLoc++;
+          break;
+        }
+      }
+      break;
+    case 2:
+      break;
+    default:
+      break;
+    }
+    //Run with set settings
+  }
+}
